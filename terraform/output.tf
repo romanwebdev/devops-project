2a8e2bc02b75ff6e36bfe8836a6a4d05acf42075
Roman <romanwebdev101@gmail.com>
2026-08-01 15:35:17 +0000

    move terraform config into terraform folder

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
   value       = aws_instance.app_server.private_dns
 }
+
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance."
+  value       = aws_instance.app_server.public_ip
+}
