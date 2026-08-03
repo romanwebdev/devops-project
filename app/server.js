19d593233c656c97d8a4ad9488bdbb2ccc110479
Roman <romanwebdev101@gmail.com>
2026-08-03 17:04:09 +0000

    update node.js service and add Dockerfile

--- app/server.js
+++ app/server.js
@@ -1,9 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Project!');
+  res.send('Hello, world!');
+});
+
+function basicAuth(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    res.set('WWW-Authenticate', 'Basic realm="Secret Area"');
+    return res.status(401).send('Authentication required.');
+  }
+
+  const base64Credentials = authHeader.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+  const [username, password] = credentials.split(':');
+
+  if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+    return next();
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="Secret Area"');
+  return res.status(401).send('Invalid username or password.');
+}
+
+app.get('/secret', basicAuth, (req, res) => {
+  res.send(process.env.SECRET_MESSAGE);
 });
 
 app.listen(PORT, () => {
