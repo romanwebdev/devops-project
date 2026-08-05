811c085638675e115669a8071218d916ec89e995
Roman <romanwebdev101@gmail.com>
2026-08-05 15:54:10 +0300

    feat: add Prometheus metrics tracking and expose /metrics endpoint

--- app/server.js
+++ app/server.js
@@ -1,10 +1,55 @@
 require('dotenv').config();
 const express = require('express');
+const {
+  Registry,
+  collectDefaultMetrics,
+  Counter,
+  Histogram,
+} = require('prom-client');
 const mongoose = require('mongoose');
 const todosRouter = require('./routes/todos');
 
 const app = express();
 const PORT = process.env.PORT || 80;
+
+// Prometheus registry
+const register = new Registry();
+collectDefaultMetrics({ register });
+
+// Custom metrics
+const httpRequestCount = new Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code'],
+  registers: [register],
+});
+
+const httpRequestDuration = new Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.01, 0.05, 0.1, 0.3, 0.5, 1, 2, 5],
+  registers: [register],
+});
+
+// Middleware to track all requests
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    const route = req.route ? req.route.path : req.path;
+    httpRequestCount.inc({
+      method: req.method,
+      route,
+      status_code: res.statusCode,
+    });
+    end({
+      method: req.method,
+      route,
+      status_code: res.statusCode,
+    });
+  });
+  next();
+});
 
 app.use(express.json());
 
@@ -15,6 +60,12 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+// Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.send(await register.metrics());
 });
 
 app.use('/todos', todosRouter);
